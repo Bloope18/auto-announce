eae3c9c55a683bf3e2e99310b23fd1f29a11441a
lahtineww <85704533+lahtineww@users.noreply.github.com>
2021-08-26 02:26:20 +0300

    Add files via upload

---
+++ auto-announce/configi.lua
@@ -0,0 +1,17 @@
+Configi = {
+    nimi = "Enchanted",
+    vari = {255,255,255}, -- R, G, B
+    viive = 10, -- sekunteina
+    viestittt = { -- samal formaatil voi lisää niin paljon ku ikinä ite haluut :D
+        [1] = {teksti = "1"},
+        [2] = {teksti = "2"},
+        [3] = {teksti = "3"},
+        [4] = {teksti = "4"},
+        [5] = {teksti = "5"},
+        [6] = {teksti = "6"},
+        [7] = {teksti = "7"},
+        [8] = {teksti = "8"},
+        [9] = {teksti = "9"},
+        [10] = {teksti = "10"}
+    }
+}
